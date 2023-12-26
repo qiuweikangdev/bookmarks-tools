8ed583f5c4de4e2d6313b3e193ed0e2721e770d5
qiuweikangdev <642827353@qq.com>
2023-12-26 21:58:23 +0800

    feat: ant-design-vue

--- src/entries/popup/main.ts
+++ src/entries/popup/main.ts
@@ -1,4 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'ant-design-vue/dist/reset.css';
 
-createApp(App).mount("#app");
+createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import webExtension from "@samrum/vite-plugin-web-extension";
-import path from "path";
-import { getManifest } from "./src/manifest";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import webExtension from '@samrum/vite-plugin-web-extension';
+import path from 'path';
+import { getManifest } from './src/manifest';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -12,10 +14,17 @@ export default defineConfig(() => {
       webExtension({
         manifest: getManifest(),
       }),
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false,
+          }),
+        ],
+      }),
     ],
     resolve: {
       alias: {
-        "~": path.resolve(__dirname, "./src"),
+        '~': path.resolve(__dirname, './src'),
       },
     },
   };
